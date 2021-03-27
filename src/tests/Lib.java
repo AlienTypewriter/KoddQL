113c2f48a535c01c980c33286cdbbfec9c038164
Voytsik <48546238+Voytsik@users.noreply.github.com>
2021-03-27 21:32:17 +0200

    Add files via upload

---
+++ src/tests/Lib.java
@@ -0,0 +1,54 @@
+package tests;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Lib {
+    public static int getLines(String fileName) {
+        int lines = 0;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            while (reader.readLine() != null) lines++;
+            reader.close();
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return lines;
+    }
+
+    public static ArrayList<String> getKoddQLCommands(String fileName) {
+        return getStrings(fileName, '\n');
+    }
+
+    public static ArrayList<String> getSQLCommands(String fileName) {
+        return getStrings(fileName, ';');
+    }
+
+    private static ArrayList<String> getStrings(String fileName, char symbol) {
+        ArrayList<String> strings = new ArrayList<String>();
+
+        try (FileReader reader = new FileReader(fileName)) {
+            int c;
+            boolean flag = false;
+            StringBuilder tempStr = new StringBuilder();
+            while ((c = reader.read()) != -1) {
+                if ((char) c == symbol) {
+                    if (symbol == ';')
+                        tempStr.append("");
+                    tempStr = new StringBuilder(tempStr.toString().trim());
+                    strings.add(tempStr.toString());
+                    tempStr = new StringBuilder();
+                    flag = true;
+                } else
+                    tempStr.append((char) c);
+            }
+            strings.add(tempStr.toString());
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        return strings;
+    }
+}
